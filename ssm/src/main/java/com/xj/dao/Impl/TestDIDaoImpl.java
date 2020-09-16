0c229417687a1608406f7f034065c36df74c56c2
DESKTOP-H5LM1HH\神圣一光-比-布里塔尼亚 <2690053948@qq.com>
2020-09-16 16:53:03 +0800

    开始阶段

---
+++ ssm/src/main/java/com/xj/dao/Impl/TestDIDaoImpl.java
@@ -0,0 +1,13 @@
+package com.xj.dao.Impl;
+
+import com.xj.dao.TestDIDao;
+
+//测试实例Dao
+public class TestDIDaoImpl implements TestDIDao{
+
+	@Override
+	public void sayHello() {
+		System.out.println("TestDIDao say : hello study hard!!!");
+	}
+
+}
