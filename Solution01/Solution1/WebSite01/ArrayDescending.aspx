e7c5416adee052f91e02d434578e04b9adcc0dfb
DESKTOP-H5LM1HH\神圣一光-比-布里塔尼亚 <2690053948@qq.com>
2020-10-07 14:05:09 +0800

    实验3-1

---
+++ Solution01/Solution1/WebSite01/ArrayDescending.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ArrayDescending.aspx.cs" Inherits="ArrayDescending" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="降序" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Solution01/Solution1/WebSite01/Division.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Division : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            float divsor = float.Parse(Divsortxt.Text);
+            float dividend = float.Parse(Dividendtxt.Text);
+            Response.Write("商为：" + divsor / dividend);
+        }
+        catch(Exception ee)
+        {
+            Response.Write("请输入正确的数字!");
+        }
+    }
+}
