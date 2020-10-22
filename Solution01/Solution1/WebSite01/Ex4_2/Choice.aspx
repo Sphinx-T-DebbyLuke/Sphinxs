ca06b18f14ac08d3ef5fc649d754afe9ef602b15
DESKTOP-H5LM1HH\神圣一光-比-布里塔尼亚 <2690053948@qq.com>
2020-10-22 19:05:04 +0800

    实验四

---
+++ Solution01/Solution1/WebSite01/Ex4_2/Choice.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Choice.aspx.cs" Inherits="Ex4_2_Choice" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="提交" />
+        <asp:Label ID="lbl_display" runat="server" Text="Label"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Solution01/Solution1/WebSite01/Ex4_2/Choice.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Ex4_2_Choice : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Label lblQuestion = new Label
+        {
+            ID = "lblQuestion",
+            Text = "1.&nbsp;Web服务器控件不包括(&nbsp;&nbsp;&nbsp;&nbsp;)"
+        };
+        PlaceHolder1.Controls.Add(lblQuestion);
+        RadioButtonList rbtChoice = new RadioButtonList
+        {
+            ID = "rbtChoice"
+        };
+        rbtChoice.Items.Add(new ListItem("A.Wizard","A"));
+        rbtChoice.Items.Add(new ListItem("B.Input", "B"));
+        rbtChoice.Items.Add(new ListItem("C.AdRotator", "C"));
+        rbtChoice.Items.Add(new ListItem("D.Calendar", "D"));
+        PlaceHolder1.Controls.Add(rbtChoice);
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        RadioButtonList rbtChoice = (RadioButtonList)PlaceHolder1.FindControl("rbtChoice");
+        lbl_display.Text = "您选择了：" + rbtChoice.SelectedValue;
+    }
+}
